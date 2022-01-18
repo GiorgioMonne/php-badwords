11297388cd8e5ddcc42618fe686b7de162e94666
Giorgio <1999giorgio.monne@mail.com>
2022-01-17 21:34:16 -1000

    inizio

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+    $nome = "Giorgio";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Intro PHP</title>
+</head>
+<body>
+
+    <h1>Ciao mi chiamo <?php echo $_GET["nome"]; ?> </h1>
+
+</body>
+</html>
